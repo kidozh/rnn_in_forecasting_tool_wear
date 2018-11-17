52bb8977fbf6ef758c2427ade210c549819152d5
kido Zhang <331837926@qq.com>
2018-11-17 19:43:40 +0800

    Initial commit

---
+++ data.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import numpy as np
+import csv
+
+
+class PHMToolWearDataset(object):
+    # extract all data
+    cache_dir_path = '.cache/'
+    total_signal_num = 315
+    # only 1,4 and 6 is correct
+    sample_label = [1, 4, 6]
+    sample_loc = 1
+
+    def __init__(self):
+        self.force_update = False
+
+    @property
+    def res_data_path(self):
+        return '/home/kidozh/PycharmProjects/nonalignment_signal_tool_wear/c%s_wear.csv' % (self.sample_loc)
+
+    @property
+    def get_res_data_in_numpy(self):
+        # remove cache because it's not needed
+        res_csv_data = self.get_res_data_by_pandas
+        res_array = np.array([np.array(i).reshape(3) for i in res_csv_data.values])
+        # np.save(storage_path, res_array)
+        return res_array
+
+    @property
+    def get_res_data_by_pandas(self):
+        return pd.read_csv(self.res_data_path, index_col='cut')
+
+    @property
+    def res_data_storage(self):
+        return 'phm_tool_wear_data'
+
+    @property
+    def get_tool_wear_data(self):
+        storage_path = self.cache_dir_path + self.res_data_storage
+        self.sample_loc = 1
+        y_dat = self.get_res_data_in_numpy
+        for i in [4, 6]:
+            self.sample_loc = i
+            y_dat = np.append(self.get_res_data_in_numpy, y_dat, axis=0)
+
+        print(y_dat.shape)
+        return y_dat
+
+
+
+
+class RNNSeriesDataSet(object):
+
+    def __init__(self,begin_timestep,end_timestep):
+        a = PHMToolWearDataset()
+        self.tool_wear_data = a.get_tool_wear_data
+        self.max_tool_wear_data = np.max(self.tool_wear_data,axis=1)
+        print(self.max_tool_wear_data.shape)
+        self.begin_timestep = begin_timestep
+        self.end_timestep = end_timestep
+
+    def get_tool_wear_data(self):
+        return self.tool_wear_data
+
+    def get_individual_tool_wear_batches(self,tool_wear_data):
+        round_number = tool_wear_data.shape[0]
+        begin_series = []
+        end_series = []
+        for start_index in range(self.begin_timestep,round_number-self.end_timestep):
+            x = tool_wear_data[start_index-self.begin_timestep:start_index]
+            y = tool_wear_data[start_index:start_index+self.end_timestep]
+            begin_series.append(x)
+            end_series.append(y)
+        return begin_series,end_series
+
+    def get_rnn_data(self):
+        x,y = [],[]
+        for i in range(3):
+            ix,iy = self.get_individual_tool_wear_batches(self.max_tool_wear_data[i*315:(i+1)*315])
+            x.extend(ix)
+            y.extend(iy)
+            # print(len(ix))
+        dat_x,dat_y =  np.array(x),np.array(y)
+        return dat_x.reshape((dat_x.shape[0],dat_x.shape[1],1)),dat_y.reshape((dat_y.shape[0],dat_y.shape[1],1))
+
+
+if __name__ == "__main__":
+    a = RNNSeriesDataSet(2,3)
+    dat_x,dat_y = a.get_rnn_data()
+    print(dat_x.shape,dat_y.shape)
---
+++ seq2seq_tool_wear/main.py
@@ -0,0 +1,93 @@
+from seq2seq_tool_wear.model import *
+from data import RNNSeriesDataSet
+from keras.callbacks import TensorBoard,ModelCheckpoint
+import os.path
+from keras.models import load_model
+import matplotlib.pyplot as plt
+
+# ---- CONF ------
+LOG_DIR = "MAX_KERAS_ROI_LOG/"
+PREDICT = True
+
+# ---- GEN Data ----
+data = RNNSeriesDataSet(2,3)
+x,y = data.get_rnn_data()
+
+# ---- shuffle -----
+import random
+# set random seeds so that we can get the same random data!
+SEED = 12347
+random.seed(SEED)
+index = [i for i in range(len(y))]
+random.shuffle(index)
+train_y = y[index]
+train_x = x[index]
+
+for DEPTH in [5,10]:
+
+    HIDDEN_DIM = 128
+    TRAIN_NAME = "Simple_RNN_Depth_%s_hidden_dim_%s" % (DEPTH,HIDDEN_DIM)
+    MODEL_NAME = "%s.kerasmodel" % (TRAIN_NAME)
+    MODEL_WEIGHT_NAME = "%s.kerasweight" % (TRAIN_NAME)
+    MODEL_CHECK_PT = "%s.kerascheckpts" % (TRAIN_NAME)
+
+    # model = build_model(1, 2, HIDDEN_DIM, 3, 1, DEPTH)
+    model = build_simple_RNN((2,1),3,1)
+    print(model.summary())
+    print("Model has been built.")
+
+    if not PREDICT:
+        print("In [TRAIN] mode")
+        # CALLBACK
+        tb_cb = TensorBoard(log_dir=LOG_DIR + TRAIN_NAME)
+        ckp_cb = ModelCheckpoint(MODEL_CHECK_PT, monitor='val_loss', save_weights_only=True, verbose=1,
+                                 save_best_only=True, period=5)
+
+        if os.path.exists(MODEL_CHECK_PT):
+            model.load_weights(MODEL_CHECK_PT)
+            print("load checkpoint successfully")
+        else:
+            print("No checkpoints found !")
+        print("Start to train the model")
+
+        model.fit(train_x,train_y,batch_size=16,epochs=1000,validation_split=0.2,callbacks=[tb_cb,ckp_cb])
+
+        model.model.save(MODEL_NAME)
+        model.save_weights(MODEL_WEIGHT_NAME)
+
+    else:
+        if os.path.exists(MODEL_CHECK_PT):
+            model.load_weights(MODEL_CHECK_PT)
+            print("load checkpoint successfully")
+        else:
+            print("No checkpoints found! try to load model directly")
+            model = load_model(MODEL_NAME)
+
+        tool_wear_data = data.max_tool_wear_data
+
+        for knife_number in range(3):
+            knife_data = tool_wear_data[knife_number*315:(knife_number+1)*315]
+            print("Current Knife shape:",knife_data.shape)
+            first_list = [None,None,]
+            second_list = [None,None,None]
+            third_list = [None,None,None,None]
+            fig = plt.figure()
+            for every_start in range(knife_data.shape[0]-3):
+                predicted = model.predict(knife_data[every_start:every_start+2].reshape(1,2,1)).reshape(3)
+
+                first_list.append(predicted[0])
+                second_list.append(predicted[1])
+                third_list.append(predicted[2])
+
+
+            plt.plot(knife_data,label="predicted")
+            plt.plot(first_list,label="1st")
+            plt.plot(second_list, label="2nd")
+            plt.plot(third_list, label="3rd")
+            plt.legend()
+            plt.show()
+
+
+
+
+
